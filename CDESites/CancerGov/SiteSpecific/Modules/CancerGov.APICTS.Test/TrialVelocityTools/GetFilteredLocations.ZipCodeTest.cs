f4d5c59df8e5015f19ad3b2e50e33c4cb8020e89
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2018-02-05 14:34:19 -0500

    [OCECTS-778] Added VA test to Zip Code Tests Also finished the test which had just been asserting true == false.

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.APICTS.Test/TrialVelocityTools/GetFilteredLocations.ZipCodeTest.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.APICTS.Test/TrialVelocityTools/GetFilteredLocations.ZipCodeTest.cs
@@ -24,16 +24,41 @@ namespace CancerGov.ClinicalTrials.Basic.v2.Test.TrialVelocityTools
                 {
                     //Empty Set
                     new object[] {
-                        LoadTrial("2AtNIH.json"), 
+                        LoadTrial("2AtNIH.json"),
                         new CTSSearchParams() {
                             Location = LocationType.Zip,
                             LocationParams = new ZipCodeLocationSearchParams() {
                                 ZipCode = "20852",
-                                ZipRadius = 10
+                                ZipRadius = 10,
+                                GeoLocation = new GeoLocation(
+                                    38.984653,
+                                    -77.094711
+                                )
+
                             }
                         },
                         //Order matters for the study sites.
                         new ClinicalTrial.StudySite[] {
+                            new ClinicalTrial.StudySite()
+                            {
+                                ContactEmail = "aekim@childrensnational.org",
+                                ContactName = "AeRang Kim",
+                                ContactPhone = "202-476-4744",
+                                RecruitmentStatus = "ACTIVE",
+                                LocalSiteIdentifier = "",
+                                AddressLine1 = "111 Michigan Avenue Northwest",
+                                City = "Washington",
+                                Country = "United States",
+                                Name = "Children's National Medical Center",
+                                OrgPhone = "202-884-2549",
+                                PostalCode = "20010",
+                                StateOrProvinceAbbreviation = "DC",
+                                Coordinates = new ClinicalTrial.StudySite.GeoLocation()
+                                {
+                                    Latitude = 38.9322,
+                                    Longitude = -77.028
+                                }
+                            },
                             new ClinicalTrial.StudySite() {
                                AddressLine1 = "10 Center Drive",
                                AddressLine2 = null,
@@ -48,7 +73,7 @@ namespace CancerGov.ClinicalTrials.Basic.v2.Test.TrialVelocityTools
                                Family = null,
                                LocalSiteIdentifier = null,
                                Name = "National Institutes of Health Clinical Center",
-                               OrgEmail = null, OrgFax = null, 
+                               OrgEmail = null, OrgFax = null,
                                OrgPhone = "800-411-1222",
                                OrgToFamilyRelationship = null,
                                OrgTTY = null,
@@ -70,7 +95,7 @@ namespace CancerGov.ClinicalTrials.Basic.v2.Test.TrialVelocityTools
                                Family = "NCI Center for Cancer Research (CCR)",
                                LocalSiteIdentifier = null,
                                Name = "National Cancer Institute Pediatric Oncology Branch",
-                               OrgEmail = null, OrgFax = null, 
+                               OrgEmail = null, OrgFax = null,
                                OrgPhone = "877-624-4878",
                                OrgToFamilyRelationship = "ORGANIZATIONAL",
                                OrgTTY = null,
@@ -78,6 +103,45 @@ namespace CancerGov.ClinicalTrials.Basic.v2.Test.TrialVelocityTools
                                RecruitmentStatus = "ACTIVE",
                                StateOrProvinceAbbreviation = "MD"
                            }
+                        }
+                    },
+                    new object[] {
+                        LoadTrial("NCIMatch.json"),
+                        new CTSSearchParams() {
+                            IsVAOnly = true,
+                            Location = LocationType.Zip,
+                            //Location as Norwalk CT.  Near VA hospital in CT, near NYC (with non VA sites)
+                            LocationParams = new ZipCodeLocationSearchParams() {
+                                ZipCode = "06850",
+                                ZipRadius = 200,
+                                GeoLocation = new GeoLocation(
+                                    41.1173,
+                                    -73.4593
+                                )
+
+                            }
+                        },
+                        //Order matters for the study sites.
+                        new ClinicalTrial.StudySite[] {
+                            new ClinicalTrial.StudySite()
+                            {
+                                ContactName = "Herta Huey-An Chao",
+                                ContactPhone = "203-937-3421ext2832",
+                                RecruitmentStatus = "ACTIVE",
+                                LocalSiteIdentifier = "",
+                                AddressLine1 = "950 Campbell Avenue",                                
+                                City = "West Haven",
+                                Country = "United States",
+                                Name = "Veterans Affairs Connecticut Healthcare System-West Haven Campus",
+                                OrgPhone = "203-932-5711",
+                                PostalCode = "06516",
+                                StateOrProvinceAbbreviation = "CT",
+                                IsVA = true,
+                                Coordinates = new ClinicalTrial.StudySite.GeoLocation() {
+                                    Latitude = 41.2716,
+                                    Longitude = -72.9666
+                                }
+                            }
                         }
                     }
                 };
