efc79e04bce47a9378ea667c6483799267060ebe
sarinapadilla <sarinapadilla@users.noreply.github.com>
2020-07-20 13:17:21 -0400

    (#3) Glossary Link Href Handler
    * Create new HttpHandler for glossary popup links
    * Add modeling for GlossaryTerm object
    * Add Glossary API Client
    * Add web.config section configuration for legacy dictionary support
    * Remove old popDefinition/definition from CancerGov.Web project
    * Add references to new project
    * Closes #3

---
+++ CDEFramework/Libraries/LegacyDictionarySupport/GlossaryAPIClient.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using Common.Logging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using LegacyDictionarySupport.Configuration;
+
+namespace LegacyDictionarySupport
+{
+    public class GlossaryAPIClient
+    {
+        static ILog log = LogManager.GetLogger(typeof(GlossaryAPIClient));
+
+        private HttpClient _client = null;
+
+        /// <summary>
+        /// Creates a new instance of a Glossary API client.
+        /// </summary>
+        /// <param name="client">An HttpClient that has the BaseAddress set to the API address.</param>
+        public GlossaryAPIClient()
+        {
+            string baseApiPath = WebAPISection.GetAPIUrl();
+            string appPath = ConfigurationManager.AppSettings["GlossaryAPIAppPath"];
+            string versionPath = ConfigurationManager.AppSettings["GlossaryAPIVersionPath"];
+
+            if (string.IsNullOrWhiteSpace(appPath))
+                throw new ConfigurationErrorsException("error: GlossaryAPIAppPath cannot be null or empty");
+
+            if (string.IsNullOrWhiteSpace(versionPath))
+                throw new ConfigurationErrorsException("error: GlossaryAPIVersionPath cannot be null or empty");
+
+            HttpClient client = new HttpClient();
+            //NOTE: the base URL MUST have a trailing slash
+            client.BaseAddress = new Uri(String.Format("{0}/{1}/{2}/", baseApiPath, appPath, versionPath));
+
+            this._client = client;
+        }
+
+        /// <summary>
+        /// Calls the GetById endpoint of the Glossary API
+        /// </summary>
+        /// <param name="dictionary">Dictionary name (required)</param>
+        /// <param name="audience">Audience type(required)</param>
+        /// <param name="language">Language to use (required)</param>
+        /// <param name="id">The ID of the term</param>
+        /// <param name="useFallback">Whether or not to use the fallback logic</param>
+        /// <returns>A Best Bet result</returns>
+        public GlossaryTerm GetById(
+            string dictionary,
+            string audience,
+            string language,
+            string id,
+            bool useFallback = false 
+            )
+        {
+            //GlossaryTerm resultTerm = null;
+
+            // Check fields
+            if (String.IsNullOrWhiteSpace(dictionary))
+            {
+                throw new ArgumentNullException("The dictionary is null or an empty string");
+            }
+            if (String.IsNullOrWhiteSpace(audience))
+            {
+                throw new ArgumentNullException("The audience is null or an empty string");
+            }
+            if (String.IsNullOrWhiteSpace(language))
+            {
+                throw new ArgumentNullException("The language is null or an empty string");
+            }
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException("The ID is null or an empty string");
+            }
+
+            // Set up search param string: {dictionary}/{audience}/{language}/{id:long}
+            string[] searchParams = { dictionary, audience, language, id, useFallback.ToString() };
+            string searchParam = string.Format("{0}/{1}/{2}/{3}?useFallback={4}", searchParams);
+
+            //Get the HTTP response content from GET request
+            HttpContent httpContent = ReturnGetRespContent("Terms", searchParam);
+            if (httpContent != null)
+            {
+                Task<GlossaryTerm> term = ReadAsJsonAsync<GlossaryTerm>(httpContent);
+                var resultTerm = term.Result;
+                return resultTerm;
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        /// <summary>
+        /// Gets the response content of a GET request.
+        /// </summary>
+        /// <param name="path">Path for client address</param>
+        /// <param name="param">Param in URL</param>
+        /// <returns>HTTP response content</returns>
+        public HttpContent ReturnGetRespContent(String path, String param)
+        {
+            HttpResponseMessage response = null;
+            HttpContent content = null;
+            String notFound = "NotFound";
+
+            
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            // We want this to be synchronus, so call Result right away.
+            //NOTE: When using HttpClient.BaseAddress as we are, the path must not have a preceeding slash
+            response = _client.GetAsync(path + "/" + param).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                content = response.Content;
+            }
+            else
+            {
+                string errorMessage = "Response: " + response.Content.ReadAsStringAsync().Result + "\nAPI path: " + this._client.BaseAddress.ToString() + path + "/" + param;
+                if (response.StatusCode.ToString() == notFound)
+                {
+                    // If trial is not found, log 404 message and return content as null
+                    log.Debug(errorMessage);
+                }
+                else
+                {
+                    // If response is other error message, log and throw exception
+                    log.Error(errorMessage);
+                    throw new Exception(errorMessage);
+                }
+            }
+
+            return content;
+        }
+
+        public static async Task<GlossaryTerm> ReadAsJsonAsync<GlossaryTerm>(HttpContent content)
+        {
+            string json = await content.ReadAsStringAsync();
+            GlossaryTerm result = JsonConvert.DeserializeObject<GlossaryTerm>(json);
+            return result;
+        }
+    }
+}
---
+++ CDEFramework/Libraries/LegacyDictionarySupport/HttpHandlers/GlossaryLinkHrefHandler.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Web;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using LegacyDictionarySupport.Configuration;
+
+namespace LegacyDictionarySupport.HttpHandlers
+{
+
+    public class GlossaryLinkHrefHandler : IHttpHandler
+    {
+        // implement IsReusable method
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+
+            // Format ID parameter for API call
+            string id = request.QueryString["id"];
+            id = Regex.Replace(id, "^CDR0+", "", RegexOptions.Compiled);
+
+            // Format dictionary and audience parameters for API call
+            string dictionary = request.QueryString["dictionary"];
+            if (!string.IsNullOrWhiteSpace(dictionary) && dictionary.ToLower() == "genetic")
+            {
+                dictionary = "Genetics";
+            }
+
+            string audience = request.QueryString["version"];
+            if (!string.IsNullOrWhiteSpace(audience) && audience.ToLower() == "patient")
+            {
+                audience = "Patient";
+            }
+            if (!string.IsNullOrWhiteSpace(audience) && audience.ToLower() == "healthprofessional")
+            {
+                audience = "HealthProfessional";
+            }
+
+            if (string.IsNullOrWhiteSpace(dictionary) && string.IsNullOrWhiteSpace(audience))
+            {
+                dictionary = "Cancer.gov";
+                audience = "Patient";
+            }
+
+            if (!string.IsNullOrWhiteSpace(audience) && string.IsNullOrWhiteSpace(dictionary))
+            {
+                switch (audience.ToLower())
+                {
+                    case "patient":
+                        dictionary = "Cancer.gov";
+                        audience = "Patient";
+                        break;
+
+                    case "healthprofessional":
+                        dictionary = "NotSet";
+                        audience = "HealthProfessional";
+                        break;
+
+                    default:
+                        dictionary = "NotSet";
+                        audience = "Patient";
+                        break;
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(dictionary) && string.IsNullOrWhiteSpace(audience))
+            {
+                audience = dictionary.ToLower() == "cancer.gov" ? "Patient" : "HealthProfessional";
+            }
+
+            // Format language parameter for API call
+            string language = request.QueryString["language"];
+            if(language == "English")
+            {
+                language = "en";
+            }
+            else if (language == "Spanish")
+            {
+                language = "es";
+            }
+
+            bool useFallback = true;
+
+            GlossaryAPIClient client = new GlossaryAPIClient();
+
+            GlossaryTerm term = client.GetById(dictionary, audience, language, id, useFallback);
+
+            if (term == null)
+            {
+                throw new HttpException(404, String.Format("Term with CDRID {0} does not exist", id));
+            }
+            else
+            {
+                string termForRedirect = String.IsNullOrEmpty(term.PrettyUrlName) ? term.TermId : term.PrettyUrlName;
+                string dictionaryPath = null;
+
+                switch (term.Dictionary.ToLower())
+                {
+                    case "cancer.gov":
+                        if (term.Language.ToLower() == "en")
+                        {
+                            dictionaryPath = LegacyDictionarySupportSection.GetEnglishTermDictionaryUrl();
+                        }
+                        if (term.Language.ToLower() == "es")
+                        {
+                            dictionaryPath = LegacyDictionarySupportSection.GetSpanishTermDictionaryUrl();
+                        }
+                        break;
+
+                    case "genetics":
+                        if (term.Language.ToLower() == "en")
+                        {
+                            dictionaryPath = LegacyDictionarySupportSection.GetEnglishGeneticsDictionaryUrl();
+                        }
+                        break;
+
+                    case "notset":
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (!string.IsNullOrWhiteSpace(dictionaryPath))
+                {
+                    context.Response.Redirect(String.Format("{0}/def/{1}", dictionaryPath, termForRedirect));
+                }
+                else
+                {
+                    context.Response.Write(@"
+<!DOCTYPE html>
+    <html lang=""" + term.Language + @""">
+    <head>
+        <title>Definition of " + term.TermName + @"</title>
+        <meta name=""robots"" content=""noindex, nofollow"" />
+        <style>
+        @import url(""https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap"");
+        .definition {
+            font-family: ""Noto Sans"";
+            margin: 30px 15px;
+        }
+        .definition__header {
+            margin-bottom: 15px;
+        }
+        .definition h1 {
+            font-size: 16px;
+            font-weight: 700;
+            display: inline-block;
+        }
+        .definition dd {
+            margin: 0;
+        }
+        .definition dd +* {
+            margin - top: 15px;
+        }
+        </style>
+    </head>");
+
+                    context.Response.Write(@"
+    <body>
+        <div class=""definition"">
+            <dl>
+            <div class=""definition__header"">
+                <a href=""/"" id=""logoAnchor"">
+                <img src=""https://www.cancer.gov/publishedcontent/images/images/design-elements/logos/nci-logo-full.svg""  id=""logoImage"" alt=""National Cancer Institute"" width=""300"" />
+                </a>
+            </div>
+            <dt>
+                <h1>" + term.TermName + @"</h1>");
+
+                    if(term.Pronunciation != null && term.Pronunciation.Key != null)
+                    {
+                        context.Response.Write(@"
+                <span class=""pronunciation"">" + term.Pronunciation.Key + @"</span>");
+                    }
+
+                    context.Response.Write(@"
+            </dt>");
+
+                    if (term.Definition != null && term.Definition.Text != null)
+                    {
+                        context.Response.Write(@"
+            <dd>" + term.Definition.Text + @"</dd>");
+                    }
+
+                    context.Response.Write(@"
+        </dl>
+        </div>
+    </body>");
+
+                    context.Response.Write(@"
+</html>");
+                }
+            }
+        }
+    }
+}
